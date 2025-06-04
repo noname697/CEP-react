8093e0279ed09eb4be452d34f8c99ae4d7c35c91
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-04 18:51:26 -0300

    Pegando valor do input com useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cep, setCep] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-
+        <input
+          placeholder="Digite o CEP"
+          onChange={(e) => setCep(e.target.value)}
+        />
+        <ul>
+          <li>CEP: {cep}</li>
+        </ul>
       </header>
     </div>
   );
